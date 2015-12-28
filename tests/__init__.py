bf002d3a69eaa3b8c765941d251b21da05bb3e26
Jachym Cepicky <jachym.cepicky@gmail.com>
2015-12-28 22:33:49 +0100

    add database logging tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,6 +5,7 @@ from tests import test_capabilities
 from tests import test_describe
 from tests import test_execute
 from tests import test_requests
+from tests import test_log
 #from tests import test_exceptions
 
 def load_tests():
@@ -12,7 +13,8 @@ def load_tests():
         test_capabilities.load_tests(),
         test_describe.load_tests(),
         test_execute.load_tests(),
-        test_requests.load_tests()
+        test_requests.load_tests(),
+        test_log.load_tests()
     ])
 
 if __name__ == "__main__":

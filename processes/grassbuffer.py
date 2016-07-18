4cf95033e4ae1e882049ede22dbe6fbbe41d5b8d
Jachym Cepicky <jachym.cepicky@gmail.com>
2016-07-18 14:10:09 +0200

    addding GRASS buffer module

---
+++ processes/grassbuffer.py
@@ -0,0 +1,48 @@
+__author__ = 'Jachym'
+
+from pywps import Process, LiteralInput, ComplexInput, ComplexOutput, Format, FORMATS
+from pywps.validator.complexvalidator import validategml
+
+from pywps.validator.mode import MODE
+
+
+class GrassBuffer(Process):
+    def __init__(self):
+        inputs = [ComplexInput('poly_in', 'Input1',
+                  supported_formats=[Format('application/gml+xml')],
+                  mode=MODE.STRICT),
+                  LiteralInput('buffer', 'Buffer', data_type='float',
+                  allowed_values=(0, 1, 10, (10, 10, 100), (100, 100, 1000)))]
+        outputs = [ComplexOutput('buff_out', 'Buffered',
+            supported_formats=[Format('application/gml+xml')])]
+
+        super(GrassBuffer, self).__init__(
+            self._handler,
+            identifier='grassbuffer',
+            version='0.1',
+            title="GRASS v.buffer",
+            abstract='This process is using GRASS GIS v.buffer module ',
+            profile='',
+            inputs=inputs,
+            outputs=outputs,
+            store_supported=True,
+            status_supported=True,
+            #grass_location="/tmp/outputs/pyws_process_GMkyxP/pywps_location"
+            grass_location="epsg:3857"
+        )
+
+    def _handler(self, request, response):
+
+        from grass.pygrass.modules import Module
+        Module('v.import',
+               input=request.inputs['poly_in'][0].file,
+               epsg=3857, output='poly', extent='input')
+        Module('v.buffer',
+               input='poly',
+               distance=request.inputs['buffer'][0].data,
+               output='buffer')
+        Module('v.out.ogr', input='buffer', output='buffer.gml', format='GML')
+
+        response.outputs['buff_out'].file = 'buffer.gml'
+
+        return response

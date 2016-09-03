dc7b1908433bd3d6446bfa5dfce700d0f33e75ca
Jachym Cepicky <jachym.cepicky@gmail.com>
2016-09-03 22:26:24 +0200

    documentation and setup updates

--- setup.py
+++ setup.py
@@ -1,16 +1,73 @@
+# Copyright (c) <year> <copyright holders>
+# 
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+with open('VERSION.txt') as ff:
+    VERSION = ff.read().strip()
+
+with open('requirements.txt') as f:
+    INSTALL_REQUIRES = f.read().splitlines()
+    INSTALL_REQUIRES.append('pywps-'+VERSION)
+
+DESCRIPTION = (
+'''PyWPS is an implementation of the Web Processing Service standard from the
+Open Geospatial Consortium. PyWPS is written in Python.
+
+PyWPS-demo is sample implementation of PyWPS server, distributed along with
+basic set of example processes and sample configuration file. It's usually used
+for testing and development purposes.
+''')
+
+KEYWORDS = 'PyWPS WPS OGC processing'
+
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Implementation of PyWPS-4 server',
-    'author': 'Luis de Sousa',
+    'description': DESCRIPTION,
+    'keywords': KEYWORDS,
+    'author': 'PyWPS PSC',
+    'license': 'MIT',
+    'platforms': 'all',
     'url': 'http://pywps.org',
     'download_url': 'https://github.com/geopython/pywps-demo',
     'author_email': 'luis.a.de.sousa@gmail.com',
-    'version': '4.0',
-    'install_requires': [],
+    'maintainer': 'Luis de Sousa',
+    'maintainer_email': 'luis.a.de.sousa@gmail.com',
+    'classifiers': [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+    'version': VERSION,
+    'install_requires': INSTALL_REQUIRES,
+    'dependency_links': [
+        'git+https://github.com/geopython/pywps.git@pywps-'+VERSION+'#egg=pywps-'+VERSION
+     ],
     'packages': ['processes', 'tests'],
     'scripts': ['demo.py'],
     'name': 'pywps-demo'
